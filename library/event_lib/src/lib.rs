965070befa54fb4d0ba0b4c541e8eb0fff4b9294
Alessandro Demofonti <demofonti.alessandro@gmail.com>
2023-08-08 17:57:05 +0200

    Shortcut ed altro

--- library/event_lib/src/lib.rs
+++ library/event_lib/src/lib.rs
@@ -1,14 +1,148 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use chrono::offset::Local;
+use druid::{
+    im::Vector,
+    image::{ImageBuffer, Rgba},
+    piet::ImageFormat,
+    AppDelegate, Data, DelegateCtx, Env, ImageBuf, Lens,
+};
+use screenshots::Screen;
+use shortcut_lib::*;
+
+/// Constant value of heigth of screen
+const H: usize = 1080;
+/// Constant value of width of screen
+const W: usize = 1920;
+
+/// Function associated to shortcut enum
+/// ___Action::NewScreenshot___. When it is execute
+/// produce in output a screenshot of the entire
+/// desired screen
+pub fn take_screenshot(screen_index: usize) -> ImageBuf {
+    let img = Screen::all().unwrap()[screen_index].capture().unwrap();
+    let x: ImageBuffer<Rgba<u8>, Vec<u8>> =
+        ImageBuffer::from_raw(W as u32, H as u32, img.rgba().clone()).unwrap();
+
+    let dt = Local::now();
+    x.save(dt.timestamp().to_string() + ".jpg")
+        .expect("Error in saving screenshot!");
+
+    let raw = Screen::all().unwrap()[screen_index]
+        .capture()
+        .unwrap()
+        .rgba()
+        .clone();
+
+    return ImageBuf::from_raw(
+        &raw[0..H * W * ImageFormat::RgbaSeparate.bytes_per_pixel()],
+        ImageFormat::RgbaSeparate,
+        W,
+        H,
+    );
+}
+
+fn function_2(num: usize) -> bool {
+    println!("Save {num}");
+    return true;
+}
+
+#[derive(Clone, Data, Lens)]
+pub struct AppState {
+    name: String,
+    buf: ImageBuf,
+    shortcut: Shortcuts,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        Self {
+            name: format!("Screenshot App"),
+            buf: ImageBuf::empty(),
+            shortcut: Shortcuts::from_file(),
+        }
+    }
+
+    pub fn get_name(&self) -> String {
+        self.clone().name
+    }
+
+    pub fn set_buf(&mut self, buf: ImageBuf) {
+        self.buf = buf;
+    }
+
+    pub fn get_buf(&self) -> ImageBuf {
+        self.clone().buf
+    }
+}
+
+pub struct EventHandler {
+    keys_pressed: Vector<druid::keyboard_types::Key>,
+}
+
+impl EventHandler {
+    pub fn new() -> Self {
+        Self {
+            keys_pressed: Vector::new(),
+        }
+    }
+}
+
+impl AppDelegate<AppState> for EventHandler {
+    fn event(
+        &mut self,
+        _ctx: &mut DelegateCtx,
+        _window_id: druid::WindowId,
+        event: druid::Event,
+        data: &mut AppState,
+        _env: &Env,
+    ) -> Option<druid::Event> {
+        match event {
+            druid::Event::KeyDown(ref key_event) => {
+                if self.keys_pressed.contains(&key_event.key.clone()) == false {
+                    self.keys_pressed.push_back(key_event.key.clone());
+                }
+
+                println!("Buffer {:?}", self.keys_pressed);
+
+                if self.keys_pressed.len() == 2 {
+                    match data.shortcut.extract_value(&self.keys_pressed) {
+                        Some(action) => match action {
+                            Action::NewScreenshot => {
+                                take_screenshot(0);
+                            }
+                            Action::Save => {
+                                function_2(3);
+                            }
+                        },
+                        None => {}
+                    }
+                }
+
+                return Some(event);
+            }
+
+            druid::Event::KeyUp(ref key_event) => {
+                let index = match self.keys_pressed.index_of(&key_event.key) {
+                    Some(i) => i,
+                    None => panic!("Key searched doesn't exist!"),
+                };
+
+                self.keys_pressed.remove(index);
+
+                println!("Buffer {:?}", self.keys_pressed);
+                return Some(event);
+            }
+
+            _ => Some(event),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    /* use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+        /* Write here */
+    } */
 }
---
+++ library/shortcut_lib/src/lib.rs
@@ -0,0 +1,203 @@
+use bincode;
+use druid::{
+    im::{OrdMap, Vector},
+    keyboard_types::Key,
+    Data
+};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::OpenOptions,
+    io::{Read, Write},
+    mem::size_of,
+};
+use Action::*;
+
+/// Simple struct that represent keys combination
+/// of an shortcut.
+#[derive(Clone, Serialize, Deserialize, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Data)]
+pub struct ShortcutKey(String, String);
+
+impl ShortcutKey {
+    pub fn new(keys: &Vector<Key>) -> Self {
+        let mut keys_clone = keys.clone();
+
+        let key_0 = match keys_clone.pop_front().unwrap() {
+            Key::Character(c) => c,
+            _ => keys[0].to_string(),
+        };
+
+        let key_1 = match keys_clone.pop_front().unwrap() {
+            Key::Character(c) => c,
+            _ => keys[1].to_string(),
+        };
+
+        return ShortcutKey(key_0, key_1);
+    }
+
+    pub fn to_string(&self) -> String {
+        return format!("{}+{}", self.0, self.1);
+    }
+}
+
+/// This emun represent in verbose mode, all the possible actions
+/// available:
+/// - new screenshot
+/// - save
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Data)]
+pub enum Action {
+    NewScreenshot,
+    Save,
+}
+
+impl Action {
+    pub fn to_string(&self) -> String {
+        match self {
+            NewScreenshot => "New screenshot".to_string(),
+            Save => "Save".to_string(),
+        }
+    }
+
+    pub fn from_str(str: &str) -> Option<Action> {
+        match str {
+            "New screenshot" => Some(NewScreenshot),
+            "Save" => Some(Save),
+            _ => None,
+        }
+    }
+}
+
+/// This struct contains the collection of all shortcut
+/// available, each entry contains keys combination and
+/// the correlated action:
+/// - new screenshot -> ctrl+n
+/// - save -> ctrl+s
+#[derive(Clone, Debug, PartialEq, Eq, Data)]
+pub struct Shortcuts {
+    map: OrdMap<ShortcutKey, Action>,
+}
+
+impl Shortcuts {
+    /// Debug and inizialization function for writing
+    /// shortcut on binary file
+    pub fn new_shortcut_to_file(keys: &Vector<Key>) {
+        let mut file = match OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open("./shortcuts")
+        {
+            Ok(f) => f,
+            Err(e) => panic!("{}", e),
+        };
+
+        file.write_all(
+            &bincode::serialize(&(
+                ShortcutKey::new(keys),
+                Action::Save, /* Mettere enum corrispondente all'azione voluta */
+            ))
+            .unwrap(),
+        )
+        .expect("File writing failed!");
+
+        file.flush().expect("File writing failed!");
+    }
+
+    /// Initialize and return the collection of shortcuts
+    /// stored in a binary file ( ___./shortcuts___ )
+    pub fn from_file() -> Self {
+        let mut shortcuts: OrdMap<ShortcutKey, Action> = OrdMap::new();
+
+        let mut file = match OpenOptions::new().read(true).open("./shortcuts") {
+            Ok(f) => f,
+            Err(e) => panic!("{}", e),
+        };
+
+        loop {
+            let mut buf: [u8; size_of::<(ShortcutKey, Action)>()] =
+                [0; size_of::<(ShortcutKey, Action)>()];
+            match file.read_exact(&mut buf) {
+                Ok(_) => {
+                    let s: (ShortcutKey, Action) = bincode::deserialize(&buf).unwrap();
+                    shortcuts.insert(s.0, s.1);
+                }
+                Err(_) => break,
+            }
+        }
+
+        return Self { map: shortcuts };
+    }
+
+    /// Extract and return the list of available shortcut keys combination
+    pub fn extract_keys(&mut self) -> Vector<String> {
+        let keys: Vector<String> = self.map.keys().cloned().map(|k| k.to_string()).collect();
+        return keys;
+    }
+
+    /// Extract and return the list of available shortcut names
+    pub fn extract_values(&mut self) -> Vector<String> {
+        let values: Vector<String> = self.map.values().cloned().map(|a| a.to_string()).collect();
+        return values;
+    }
+
+    /// Allows you to change a key combination for
+    /// a given shortcut and save it in the shortcut
+    /// configuration file ( ___./shortcuts___ )
+    pub fn edit_shortcut(&mut self, old_shortcut: ShortcutKey, pressed_keys: &Vector<Key>) -> bool {
+        match self.map.contains_key(&old_shortcut) {
+            true => {
+                let new_shortcut = ShortcutKey::new(pressed_keys);
+
+                if self.map.contains_key(&new_shortcut) == true {
+                    return false;
+                }
+
+                let function = self.map.remove(&old_shortcut).unwrap();
+
+                self.map.insert(new_shortcut, function);
+
+                let mut file = match OpenOptions::new()
+                    .write(true)
+                    .truncate(true)
+                    .open("./shortcuts")
+                {
+                    Ok(f) => f,
+                    Err(e) => panic!("{}", e),
+                };
+
+                let map_clone = self.map.clone();
+                for s in map_clone.into_iter() {
+                    file.write_all(&bincode::serialize(&s).unwrap())
+                        .expect("File writing failed!");
+                }
+
+                file.flush().expect("File writing failed!");
+
+                return true;
+            }
+            false => {
+                panic!("Shortcut doesn't exist!");
+            }
+        }
+    }
+
+    /// Given a key combination, it return the associeted
+    /// action of the shortcut, if it exists
+    pub fn extract_value(&self, keys: &Vector<Key>) -> Option<Action> {
+        let s = ShortcutKey::new(keys);
+
+        match self.map.get(&s).cloned() {
+            Some(a) => Some(a),
+            None => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    /* use super::*;
+
+    #[test]
+    fn it_works() {
+        /* Write here */
+    } */
+}
