29ef3d753dd92113c95e48851d208f26957b0920
AlessandroBelasri <belalessandro1@gmail.com>
2023-09-07 15:51:17 +0200

    Soos di oggi 2

--- library/screenshot_lib/src/lib.rs
+++ library/screenshot_lib/src/lib.rs
@@ -23,11 +23,15 @@ pub fn take_screenshot(current_screen: usize) -> ImageBuffer<Rgba<u8>, Vec<u8>> 
   This function recieve the current screen on witch the screenshot has to be taken,
   In this case, the area is also passed after a drag&drop in order to take a restricted area
   then it saves a screenshot of the whole selected area in a ImageBuffer
-  It returns an Option so, in case the selected area is bigger than the current selected screen, None is returned 
+  It returns an Option so, in case the selected area is bigger than the current selected screen, None is returned
 
 */
 
-pub fn take_screenshot_area(current_screen: usize, start_coords: (i32, i32), end_coords: (i32, i32)) -> Option<ImageBuffer<Rgba<u8>, Vec<u8>>> {
+pub fn take_screenshot_area(
+    current_screen: usize,
+    start_coords: (i32, i32),
+    end_coords: (i32, i32),
+) -> Option<ImageBuffer<Rgba<u8>, Vec<u8>>> {
     let screens = Screen::all().unwrap();
     let current_screen = screens[current_screen];
     let screen_infos = current_screen.display_info;
@@ -46,12 +50,41 @@ pub fn take_screenshot_area(current_screen: usize, start_coords: (i32, i32), end
 /*
 
   This function verifies if the drag&drop comes from left to right, form top to bottom or viceversa, then i calculates
-  the top left corner and verifies if the dimensions of the area are compatibles with the current screen 
+  the top left corner and verifies if the dimensions of the area are compatibles with the current screen
 
 */
 
-pub fn calculate_area( screen_infos: DisplayInfo, start_coords: (i32, i32), end_coords: (i32, i32)) -> Option<(i32, i32, i32, i32)> {
+pub fn calculate_area(screen_infos: DisplayInfo, mut start_coords: (i32, i32), mut end_coords: (i32, i32)) -> Option<(i32, i32, i32, i32)> {
     let mut left_corner = (0, 0);
+    // the screenshot area is between the current screen and a screen on his left
+    if start_coords.0 < 0 {
+        start_coords.0 = 0;
+    }
+    if end_coords.0 <0 {
+        end_coords.0 = 0;
+    }
+    // the screenshot area is between the current screen and a screen on his right 
+    if start_coords.0 > screen_infos.width as i32{
+        start_coords.0 = screen_infos.width as i32;
+    }
+    if end_coords.0 > screen_infos.width as i32 {
+        end_coords.0 = screen_infos.width as i32;
+    }
+    // the screenshot area is between the current screen and a screen on his top
+    if start_coords.1 < 0 {
+        start_coords.1 = 0;
+    }
+    if end_coords.1 <0 {
+        end_coords.1 = 0;
+    }
+    // the screenshot area is between the current screen and a screen on his right 
+    if start_coords.1 > screen_infos.height as i32{
+        start_coords.1 = screen_infos.height as i32;
+    }
+    if end_coords.1 > screen_infos.height as i32 {
+        end_coords.1 = screen_infos.height as i32;
+    }
+
     let x_diff = start_coords.0 - end_coords.0;
     let y_diff = start_coords.1 - end_coords.1;
     let width;
@@ -62,7 +95,7 @@ pub fn calculate_area( screen_infos: DisplayInfo, start_coords: (i32, i32), end_
         width = x_diff;
     } else {
         left_corner.0 = start_coords.0;
-        width =- x_diff;
+        width = -x_diff;
     }
     // from bottom to top
     if y_diff > 0 {
@@ -70,10 +103,11 @@ pub fn calculate_area( screen_infos: DisplayInfo, start_coords: (i32, i32), end_
         height = y_diff;
     } else {
         left_corner.1 = start_coords.1;
-        height =- y_diff;
+        height = -y_diff;
     }
     // if the top left corner + the 2 dimension are bigger than the screen sizes the screenshot is NOT valid
-    if (width + left_corner.0) > screen_infos.width as i32 || (height + left_corner.1) > screen_infos.height as i32
+    if (width + left_corner.0) > screen_infos.width as i32
+        || (height + left_corner.1) > screen_infos.height as i32
     {
         return None;
     }
